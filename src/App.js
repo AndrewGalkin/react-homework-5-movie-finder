95a07aef98a6bc9c262fea4657ec511124efb128
Andrew Galkin <andrushka@MacBook-Air-Andrew.local>
2021-11-01 15:12:33 +0200

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Switch} from "react-router-dom";
+import {Route} from "react-router-dom";
 import NavigationBar from "./components/Navigation/Navigation";
 import MovieView from "./pages/MovieView/MovieView";
 import MovieCastView from "./pages/MovieCastView/MovieCastView";
@@ -7,7 +7,6 @@ import MovieSearchBar from "./pages/MovieSearch/MovieSearchBar";
 import {useState} from "react";
 import MovieSearchRender from "./pages/MovieSearch/MovieSearchRender";
 import MovieTopList from "./pages/MovieTrends/MovieTopLIst";
-import NotFound from "./components/Error/Error";
 
 
 export default function App() {
@@ -20,22 +19,21 @@ export default function App() {
   return (
     <div>
       <NavigationBar/>
-      <Switch>
+
         <Route path="/" exact component={MovieTopList}>
         </Route>
-        <Route path="/movie" exact>
-          <MovieSearchBar onSubmit={handleSearch}/>
-          <MovieSearchRender query={query}/>
-        </Route>
-        <Route path="/movie/:movieId" exact component={MovieView}>
-        </Route>
-        <Route path="/movie/:movieId/cast" exact component={MovieCastView}>
-        </Route>
+      <Route path="/movie" exact>
+        <MovieSearchBar onSubmit={handleSearch}/>
+        <MovieSearchRender query={query}/>
+      </Route>
+      <Route path="/movie/:movieId" component={MovieView}>
+      </Route>
+      <Route path="/movie/:movieId/cast" exact component={MovieCastView}>
+      </Route>
         <Route path="/movie/:movieId/reviews" exact component={MovieReviews}>
         </Route>
-        <Route component={NotFound}>
-        </Route>
-      </Switch>
+
+
     </div>
   )
 }
